44deb360336a94c6e41b9a2fcb61924dc6daa841
jesus tomas <jesus@bobylash.jesus>
2021-10-18 11:18:06 -0400

    docker add

--- table.sql
+++ table.sql
@@ -8,10 +8,22 @@ COMMENT on DATABASE bd_yofio is 'base de datos para empresa yofio';
 drop table if exists credit_asignacion;
 create table credit_asignacion(
   id integer GENERATED ALWAYS AS IDENTITY PRIMARY key, 
-  monto INTEGER,
+  monto INTEGER not null,
   type_300 INTEGER,
   type_500 INTEGER,
   type_700 INTEGER,
-  exito BOOLEAN
+  exito BOOLEAN not null
 );
 
+
+select*from credit_asignacion;
+
+
+select
+  count(*) solicitudes,
+  count(case when exito='True' THEN 1 end) as exito_total,
+  count(case when exito='False' THEN 1 end) as no_exito_total,
+  avg(case when exito='True' THEN monto end) as inversion_exitosa,
+  avg(case when exito='False' THEN monto end) as inversion_no_exitosa
+from credit_asignacion
+;
